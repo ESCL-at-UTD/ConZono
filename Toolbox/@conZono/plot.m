21d4425088469c8a315e6678d1ea6b461360d8e3
ddlmcp <ddlmcp@gmail.com>
2020-10-01 18:14:07 -0500

    Plot command improved/corrected.

--- Toolbox/@conZono/plot.m
+++ Toolbox/@conZono/plot.m
@@ -6,7 +6,7 @@ function plot(obj,varargin)
 % alpha: transparency of the color defined
 % Default color: red. Default alpha: 1. Default dims: [1 2 3], [1 2], [1]
 % for 3-, 2- and 1-dimensional objects respectively.
-% 
+%
 % Usage options:
 % obj.plot([1 2])
 % obj.plot('b',0.1)
@@ -14,49 +14,47 @@ function plot(obj,varargin)
 %
 
 obj.getDimensions;
+needsProjection = true;
+
 % input checking
 if isempty(varargin)
     if obj.n > 3
-        error(['Cannot plot sets in ',num2str(obj.n),' dimensions!']);
-    else
-            color = 'r'; alpha = 1;
-            if obj.n == 3
-                dims = [1 2 3];
-            elseif obj.n ==2
-                dims = [1 2];
-            else
-                dims = 1;
-            end
+        error(['Cannot plot sets in ',num2str(obj.n),' dimensions! Please, specify dimensions to project into.']);
     end
+    color = 'r'; alpha = 1;
+    needsProjection = false;
+    
 elseif length(varargin) == 1
     dims = varargin{1};
-    if ~(isinteger(dims) && size(dims,1)==1)
-        error('Invalid input arguments.');
-    end
+    color = 'r'; alpha = 1;
     
 elseif length(varargin) == 2
-    color = varargin{1}; alpha = varargin{2};
-      if obj.n == 3
-                dims = [1 2 3];
-            elseif obj.n ==2
-                dims = [1 2];
-            else
-                dims = 1;
-      end
-elseif length(varargin) == 3
-     dims = varargin{1}; color = varargin{2}; alpha = varargin{3}; 
-      if ~(isinteger(dims) && size(dims,1)==1)
-        error('Invalid input arguments.');
+    if obj.n > 3
+        error(['Cannot plot sets in ',num2str(obj.n),' dimensions! Please, specify dimensions to project into.']);
     end
+    
+    color = varargin{1}; alpha = varargin{2};
+    needsProjection = false;
+    
+elseif length(varargin) == 3
+    dims = varargin{1}; color = varargin{2}; alpha = varargin{3};
+    
 end
 
 
-    if obj.nC == 0
-        Box = Polyhedron('lb',-ones(obj.nG,1),'ub',ones(obj.nG,1));
-    else
-        Box = Polyhedron('lb',-ones(obj.nG,1),'ub',ones(obj.nG,1),'He',[obj.A obj.b]);
-    end
-    P = plus(obj.c,affineMap(Box,obj.G));
+if obj.nC == 0
+    Box = Polyhedron('lb',-ones(obj.nG,1),'ub',ones(obj.nG,1));
+else
+    Box = Polyhedron('lb',-ones(obj.nG,1),'ub',ones(obj.nG,1),'He',[obj.A obj.b]);
+end
+
+%P = plus(obj.c,affineMap(Box,obj.G));
+P = obj.c + obj.G*Box;
+
+if needsProjection
     plot(P.projection(dims),'color',color,'alpha',alpha);
+else
+    plot(P,'color',color,'alpha',alpha);
+end
 
 end
